b8ce5cce2766e88fc66993b162d818fd8389a7f7
PaulPidou <pidou.paul@gmail.com>
2019-07-18 19:45:45 +0200

    Add unit tests

--- test/utils.js
+++ test/utils.js
@@ -6,11 +6,11 @@ import 'sinon-mongoose'
 
 import Recipe from '../models/recipe'
 import User from '../models/user'
-import { handleListDependencies } from '../utils/user'
+import { getItemsFromShoppingList, getItemsFromFridge, handleListDependencies } from '../utils/user'
 
 chai.should()
 
-describe('utils/users', function() {
+describe('utils/users - handleListDependencies', function() {
     afterEach(function() {
         Recipe.find.restore()
         User.findById.restore()
@@ -87,8 +87,131 @@ describe('utils/users', function() {
         user.shoppingList.should.have.lengthOf(4)
     })
 
+    it('handleListDependencies(SAVE_RECIPES) - Complex', async function() {
+        const user = {
+            _id: "5d1f7df9df5a80e5e1fd3eb6",
+            savedRecipes: [{
+                recipeID: "5d24c810fee493ef6c987d6f",
+                savingDate: 1563386356410
+            }],
+            fridge: [{
+                ingredientID: "5d24c811fee493ef6c987d72",
+                ingredientName: "beurre",
+                quantities: [{ unit:"g", quantity: 5 }]
+            }],
+            shoppingList: [{
+                ingredientID: "5d24c810fee493ef6c987d70",
+                ingredientName: "tortilla",
+                quantities: [{ unit: "", quantity: 1 }]
+            }, {
+                ingredientID: "5d24c811fee493ef6c987d71",
+                ingredientName: "abricot",
+                quantities: [{ unit: "", quantity: 6 }]
+            },{
+                ingredientID: "5d24c811fee493ef6c987d72",
+                ingredientName: "beurre",
+                quantities: [{ unit:"g", quantity: 5 }]
+            }, {
+                ingredientID: "5d24c811fee493ef6c987d73",
+                ingredientName: "sucre vanillé",
+                quantities: [{ unit:"sachet", quantity: 1 }]
+            }],
+            save: function() {}
+        }
+        const recipes = [
+            {
+                _id: "5d24c810fee493ef6c987d6f",
+                ingredients: [
+                    {
+                        "ingredientName": "tortilla",
+                        "unit": "",
+                        "quantity": 1,
+                        "ingredientID": "5d24c810fee493ef6c987d70"
+                    },
+                    {
+                        "ingredientName": "abricot",
+                        "unit": "",
+                        "quantity": 6,
+                        "ingredientID": "5d24c811fee493ef6c987d71"
+                    },
+                    {
+                        "ingredientName": "beurre",
+                        "unit": "g",
+                        "quantity": 10,
+                        "ingredientID": "5d24c811fee493ef6c987d72"
+                    },
+                    {
+                        "ingredientName": "sucre vanillé",
+                        "unit": "sachet",
+                        "quantity": 2,
+                        "ingredientID": "5d24c811fee493ef6c987d73"
+                    }
+                ]
+            },
+            {
+                _id: "5d24c850fee493ef6c987d77",
+                ingredients: [
+                    {
+                        "ingredientName": "oeuf",
+                        "unit": "",
+                        "quantity": 3,
+                        "ingredientID": "5d24c865fee493ef6c987d82"
+                    },
+                    {
+                        "ingredientName": "abricot",
+                        "unit": "g",
+                        "quantity": 400,
+                        "ingredientID": "5d24c811fee493ef6c987d71"
+                    },
+                    {
+                        "ingredientName": "beurre",
+                        "unit": "g",
+                        "quantity": 10,
+                        "ingredientID": "5d24c811fee493ef6c987d72"
+                    }
+                ]
+            }
+        ]
+
+        const mockFind = {
+            select: function () {
+                return this
+            },
+            exec: sinon.stub().resolves([recipes[0]])
+        }
+
+        const s = sinon.stub(Recipe, 'find')
+        s.withArgs({_id: { $in: [recipes[0]._id] }})
+            .returns(mockFind)
+
+        s.withArgs({_id: { $in: recipes.map(r => r._id) }})
+            .returns({
+                ...mockFind,
+                exec: sinon.stub().resolves(recipes)
+            })
+
+        sinon.mock(User)
+            .expects('findById')
+            .withArgs(user._id)
+            .resolves(user)
+
+        await handleListDependencies(user, 'SAVE_RECIPES', [recipes[1]._id])
+        user.savedRecipes.should.have.lengthOf(2)
+        user.shoppingList.should.have.lengthOf(5)
+        user.shoppingList[0].ingredientName.should.be.equal('tortilla')
+        user.shoppingList[0].quantities.should.deep.equal([{ unit: '', quantity: 1 }])
+        user.shoppingList[1].ingredientName.should.be.equal('abricot')
+        user.shoppingList[1].quantities.should.deep.equal([{ unit: '', quantity: 6 }, { unit: 'g', quantity: 400 }])
+        user.shoppingList[2].ingredientName.should.be.equal('beurre')
+        user.shoppingList[2].quantities.should.deep.equal([{ unit: 'g', quantity: 15 }])
+        user.shoppingList[3].ingredientName.should.be.equal('sucre vanillé')
+        user.shoppingList[3].quantities.should.deep.equal([{ unit: 'sachet', quantity: 1 }])
+        user.shoppingList[4].ingredientName.should.be.equal('oeuf')
+        user.shoppingList[4].quantities.should.deep.equal([{ unit: '', quantity: 3 }])
+    })
+
     it('handleListDependencies(REMOVE_RECIPES) - Basic', async function() {
-        let user = {
+        const user = {
             _id: "5d1f7df9df5a80e5e1fd3eb6",
             savedRecipes:[{
                 recipeID: "5d24c810fee493ef6c987d6f",
@@ -180,5 +303,49 @@ describe('utils/users', function() {
 
         await handleListDependencies(user, 'REMOVE_RECIPES', [recipes[0]._id])
         user.shoppingList.should.have.lengthOf(0)
+    })
+})
+
+describe('utils/users - others', function() {
+    it('getItemsFromShoppingList()', function() {
+        const user = {
+            shoppingList: [{
+                ingredientID: "5d24c810fee493ef6c987d70",
+                ingredientName: "tortilla",
+                quantities: [{ unit: "", quantity: 1 }]
+            }, {
+                ingredientID: "5d24c811fee493ef6c987d71",
+                ingredientName: "abricot",
+                quantities: [{ unit: "", quantity: 6 }]
+            }]
+        }
+        const items = ["5d24c811fee493ef6c987d71"]
+
+        const result = getItemsFromShoppingList(user, items)
+        result.should.have.lengthOf(1)
+        result[0].should.be.a('object')
+        result[0].ingredientID.should.be.equal('5d24c811fee493ef6c987d71')
+        result[0].ingredientName.should.be.equal('abricot')
+    })
+
+    it('getItemsFromFridge()', function() {
+        const user = {
+            fridge: [{
+                ingredientID: "5d24c810fee493ef6c987d70",
+                ingredientName: "tortilla",
+                quantities: [{ unit: "", quantity: 1 }]
+            }, {
+                ingredientID: "5d24c811fee493ef6c987d71",
+                ingredientName: "abricot",
+                quantities: [{ unit: "", quantity: 6 }]
+            }]
+        }
+        const items = ["5d24c811fee493ef6c987d71"]
+
+        const result = getItemsFromFridge(user, items)
+        result.should.have.lengthOf(1)
+        result[0].should.be.a('object')
+        result[0].ingredientID.should.be.equal('5d24c811fee493ef6c987d71')
+        result[0].ingredientName.should.be.equal('abricot')
     })
 })

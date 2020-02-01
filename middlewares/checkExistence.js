5e8ce50e05a5f5a4a3b593afeb3b6e150831db7d
PaulPidou <pidou.paul@gmail.com>
2020-02-01 11:47:51 +0100

    Add checkIfIngredientsAndSubstitutesExist

--- middlewares/checkExistence.js
+++ middlewares/checkExistence.js
@@ -13,10 +13,58 @@ exports.checkIfIngredientsExist = function(req, res, next) {
 
     Ingredient.find({_id: { $in: ids }}).exec(function(err, ingredients) {
         if(err || !ingredients) {
-            res.status(404).json({message: "Ingredients not found"})
+            res.status(404).json({ message: "Ingredients not found" })
             return
         }
         res.locals.ingredients = ingredients
+        return next()
+    })
+}
+
+exports.checkIfIngredientsAndSubstitutesExist = function(req, res, next) {
+    let ingredients = req.body.ingredients
+
+    if(!ingredients || !ingredients.length) {
+        res.status(422).json({message: "Invalid input"})
+        return
+    }
+
+    let ids = []
+    for (const ingredient of ingredients) {
+        if(typeof ingredient !== 'object') {
+            res.status(422).json({message: "Invalid input"})
+            return
+        }
+        ids.push(ingredient.ingredientID)
+        ids.push(...ingredient.substitutes.map(item => item.ingredientID))
+    }
+
+    Ingredient.find({_id: { $in: [...new Set(ids)] }}).select({'name': 1}).exec(function(err, ingredients) {
+        if(err || !ingredients) {
+            res.status(404).json({ message: "Ingredients not found" })
+            return
+        }
+
+        let ingredientsObj = {}
+        for(const ingredient of ingredients) {
+            ingredientsObj[ingredient._id] = ingredient.name
+        }
+
+        let ingredientsWithSubstitutes = []
+        for (const ingredient of req.body.ingredients) {
+            ingredientsWithSubstitutes.push({
+                ingredientID: ingredient.ingredientID,
+                substitutes: ingredient.substitutes.map((item) => {
+                    return {
+                        ingredientID: item.ingredientID,
+                        ingredientName: ingredientsObj[item.ingredientID],
+                        similarityScore: item.similarityScore
+                    }
+                })
+            })
+        }
+
+        res.locals.ingredients = ingredientsWithSubstitutes
         return next()
     })
 }

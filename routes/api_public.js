f065138d2d30944eb837ecc32d3f9c6fcba07275
PaulPidou <pidou.paul@gmail.com>
2020-02-14 14:12:00 +0100

    Small refactoring
    Remove useless import

--- routes/api_public.js
+++ routes/api_public.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import passport from 'passport'
 import jwt from 'jsonwebtoken'
-import mongoose from 'mongoose'
 
 import User from '../models/user'
 import Recipe from '../models/recipe'
